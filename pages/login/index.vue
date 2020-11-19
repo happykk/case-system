10bbc19de98b2274b2b1a2559c8d81fd9e567438
孙佳 <1149116001@qq.com>
2020-11-19 23:23:34 +0800

    谷歌人机验证

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,15 +1,19 @@
 <template>
   <section class="login-container">
     <div class="login-box">
-      <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" class="demo-ruleForm">
+      <el-form :model="loginForm" status-icon :rules="rules" ref="loginForm" class="demo-ruleForm">
         <el-form-item prop="username">
-          <el-input placeholder="请输入邮箱" v-model="ruleForm.username" autocomplete="off"></el-input>
+          <el-input placeholder="请输入邮箱" v-model="loginForm.username" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item prop="password">
-          <el-input type="password" placeholder="请输入密码" v-model="ruleForm.password" autocomplete="off"></el-input>
+          <el-input type="password" placeholder="请输入密码" v-model="loginForm.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item prop='validateCode'>
+          <reCaptcha :sitekey="sitekey" @getValidateCode='getValidateCode' v-model="loginForm.validateCode"></reCaptcha>
+         <!-- <vue-recaptcha :sitekey="sitekey" :loadRecaptchaScript="true"></vue-recaptcha> -->
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+          <el-button type="primary" @click="submitForm('loginForm')">提交</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -17,12 +21,15 @@
 </template>
 <script>
 import axios from 'axios'
+import reCaptcha from './reCaptcha'
+// import VueRecaptcha from 'vue-recaptcha';
 export default {
   // 下面的两种方式都可以；layout属性其实是将这个vue页面跟布局vue进行关联的属性，layout的值为layout文件夹下所对应布局的名字
   // layout: 'custom_layout',
   layout: function (context) {
     return "login";
   },
+  components: { reCaptcha },
   data (){
     var validatePass = (rule, value, callback) => {
       if (value === '') {
@@ -31,11 +38,21 @@ export default {
         callback();
       }
     };
+    var checkCode = (rule, value, callback) => {
+      if (value == false) {
+        callback(new Error('请进行人机验证'));
+      } else {
+        callback();
+      }
+    };
     return{
-      ruleForm: {
+      loginForm: {
         username: '',
-        password: ''
+        password: '',
+        redomStr: '',
+        validateCode: false
       },
+      sitekey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
       rules: {
         username: [
           { required: true, message: '请输入邮箱地址', trigger: 'blur' },
@@ -43,24 +60,35 @@ export default {
         ],
         password: [
           { validator: validatePass, trigger: 'blur' }
+        ],
+        validateCode: [
+          { validator: checkCode, trigger: 'change' }
         ]
       }
     }
   },
   methods: {
+    callBack (res) {
+      console.log(res)
+    },
+    getValidateCode(value) {
+      this.loginForm.validateCode = value
+      console.log(value)
+    },
     submitForm () {
       // 405925123@qq.com 824655qqq
-      this.$refs.ruleForm.validate((valid) => {
+      this.$refs.loginForm.validate((valid) => {
         if (valid) {
-          axios('http://106.52.85.160/api/login', this.ruleForm).then((data) => {
+          axios('http://106.52.85.160/api/login', this.loginForm).then((data) => {
             this.$router.back()
           })
         }
       })
      
     }
+  },
+  mounted () {
   }
-		
 };
 </script>
 <style scoped>
